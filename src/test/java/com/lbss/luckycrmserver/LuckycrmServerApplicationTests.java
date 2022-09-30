5a712e4fb4498752765bd3b0813c9022d94e543b
Vinicius Bretzke <vinicius@createsoftware.com.br>
2022-09-30 14:16:02 -0300

    First commit

---
+++ src/test/java/com/lbss/luckycrmserver/LuckycrmServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lbss.luckycrmserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuckycrmServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
