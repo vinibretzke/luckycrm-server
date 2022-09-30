903011ec049251d306d65e1de441ae2fac24f658
Vinicius Bretzke <vinicius@createsoftware.com.br>
2022-09-30 17:14:57 -0300

    Implementing DB integration

---
+++ src/main/java/com/lbss/luckycrmserver/model/Users.java
@@ -0,0 +1,12 @@
+package com.lbss.luckycrmserver.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class Users {
+
+    private Long id;
+
+}
