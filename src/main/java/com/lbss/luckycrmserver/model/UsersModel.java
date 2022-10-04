265557e367f21714e631a486e249476cbfc41800
Vinicius Bretzke <vinicius@createsoftware.com.br>
2022-10-04 20:24:54 -0300

    Implemented password encryption.

--- src/main/java/com/lbss/luckycrmserver/model/UsersModel.java
+++ src/main/java/com/lbss/luckycrmserver/model/UsersModel.java
@@ -1,14 +1,11 @@
 package com.lbss.luckycrmserver.model;
 
-import lombok.Getter;
-import lombok.Setter;
 
 import javax.persistence.*;
+import java.security.MessageDigest;
 
 @Entity
 @Table(name = "users")
-@Getter
-@Setter
 public class UsersModel {
 
     @Id
@@ -22,4 +19,80 @@ public class UsersModel {
     private String cpf;
     private String dateOfBirth;
 
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        //set encrypted password
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(password.getBytes());
+            byte[] bytes = md.digest();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < bytes.length; i++) {
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            this.password = sb.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
 }
