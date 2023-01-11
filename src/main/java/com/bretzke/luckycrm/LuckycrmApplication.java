b6c796e856ad2d2b4d022f520e4c0e72be6bca1a
Vinicius Bretzke <vinicius@createsoftware.com.br>
2023-01-11 19:32:29 -0300

    First commit.

---
+++ src/main/java/com/bretzke/luckycrm/LuckycrmApplication.java
@@ -0,0 +1,13 @@
+package com.bretzke.luckycrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckycrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuckycrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bretzke/luckycrm/LuckycrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bretzke.luckycrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuckycrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
